4296f1cb43dfc1524e96e35a1c724debc7517884
Caltonek <szymonbarton482@gmail.com>
2026-08-08 22:13:39 +0200

    upload

---
+++ src/main/java/pl/caltonek/chatFusion/ChatFusion.java
@@ -0,0 +1,16 @@
+package pl.caltonek.chatFusion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatFusion extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
